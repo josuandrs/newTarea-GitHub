adfd6459189883b934c497481a332bcdc87ef274
Josue <jasovi@gmail.com>
2023-07-27 07:15:52 -0500

    correcion 2

--- src/Tarjeta.jsx
+++ src/Tarjeta.jsx
@@ -1,10 +1,23 @@
+import './Tarjeta.css'
+
+    function BloqueImg(props){
+
+        return(
+        <div>
+            <picture>
+                <img src={props.urlImagen} alt="fondo 1" />
+            </picture>
+        </div>
+        )
+    }
+
+
 export default function Tarjeta() {
+  const fondo=['https://images.unsplash.com/photo-1690320276492-08823be529d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80']
+
   return (
     <div>
-        <picture>
-            <img src="https://images.unsplash.com/photo-1690320276492-08823be529d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80" alt="" />
-            <img src="https://images.unsplash.com/photo-1690275176891-4718337fd788?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
-        </picture>
+        <BloqueImg urlImagen={fondo[0]}/>
     </div>
   )
 }
