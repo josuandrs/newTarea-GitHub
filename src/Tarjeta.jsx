32a3332e24359184789bcde4b1fdf1a5e4e6ae62
Josue <jasovi@gmail.com>
2023-07-27 06:32:33 -0500

    seleccion imagenes

---
+++ src/Tarjeta.jsx
@@ -0,0 +1,10 @@
+export default function Tarjeta() {
+  return (
+    <div>
+        <picture>
+            <img src="https://images.unsplash.com/photo-1690320276492-08823be529d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80" alt="" />
+            <img src="https://images.unsplash.com/photo-1690275176891-4718337fd788?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+        </picture>
+    </div>
+  )
+}
